2736be9dc7a038b347fadf12cef5f21b424d5042
Sophie Reynolds <shreynolds101@gmail.com>
2019-09-28 16:46:17 -0700

    added the code for the intake

--- TeamBFirstProgram/include/config.h
+++ TeamBFirstProgram/include/config.h
@@ -3,4 +3,6 @@ vex::motor leftBackMotor(vex::PORT1,true);
 vex::motor leftMiddleMotor(vex::PORT2,true);
 vex::motor rightBackMotor(vex::PORT3,true);
 vex::motor rightMiddleMotor(vex::PORT4,true);
+vex::motor leftIntakeMotor(vex::PORT5);
+vex::motor rightIntakeMotor(vex::PORT6, true);
 vex::controller Controller1 = vex::controller();
--- TeamBFirstProgram/src/main.cpp
+++ TeamBFirstProgram/src/main.cpp
@@ -18,14 +18,29 @@ using namespace vex;
 
 int main() 
 {
+  int armSpeedPct = 50; //the percent speed of the arm
+
   while(true)
   {
-    //
+    //code for the wheels
     leftBackMotor.spin(directionType::fwd, (-Controller1.Axis4.value() - Controller1.Axis3.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
     leftMiddleMotor.spin(directionType::fwd, (-Controller1.Axis4.value() - Controller1.Axis3.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
 
     rightBackMotor.spin(directionType::fwd, (-Controller1.Axis4.value() + Controller1.Axis3.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
     rightMiddleMotor.spin(directionType::fwd, (-Controller1.Axis4.value() + Controller1.Axis3.value())/2, velocityUnits::pct); //(Axis3+Axis4)/2;
+
+
+    //code for the intake
+    if (Controller1.ButtonR1.pressing()){
+      leftIntakeMotor.spin(vex::directionType::fwd, armSpeedPct, vex::velocityUnits::pct);
+      rightIntakeMotor.spin(vex::directionType::fwd, armSpeedPct, vex::velocityUnits::pct);
+    } if (Controller1.ButtonR2.pressing()){
+      leftIntakeMotor.spin(vex::directionType::rev, armSpeedPct, vex::velocityUnits::pct);
+      rightIntakeMotor.spin(vex::directionType::rev, armSpeedPct, vex::velocityUnits::pct);
+    } else {
+      leftIntakeMotor.stop(vex::brakeType::brake);
+      rightIntakeMotor.stop(vex::brakeType::brake);
+    }
     task::sleep(20);
   }
 }
