d0d7e823bcfdddb7cc7696ded0ed5a98f9763589
teddyliang <teddytduoliang@gmail.com>
2019-09-30 17:14:25 -0700

    worked more on controller connections

--- TeamBFirstProgram/src/main.cpp
+++ TeamBFirstProgram/src/main.cpp
@@ -13,6 +13,7 @@ using namespace vex;
 int inTakeSpeed = 50;
 int rampMovementTime = 1;
 int topSpeed = 100;
+int liftIntakesSpeed = 100;
 // A global instance of vex::brain used for printing to the V5 brain screen
 
 // define your global instances of motors and other devices here
@@ -20,12 +21,44 @@ int topSpeed = 100;
 void moveRampUp()
 {
     ramp.rotateFor(rampMovementTime,timeUnits::sec,topSpeed,velocityUnits::pct);
+    task::sleep(20);
     return;
 }
 
 void moveRampDown()
 {
-    ramp.rotateFor(rampMovementTime,timeUnits::sec,topSpeed,velocityUnits::pct);
+    ramp.rotateFor(rampMovementTime,timeUnits::sec,-topSpeed,velocityUnits::pct);
+    task::sleep(20);
+    return;
+}
+
+void spinIntakes()
+{
+    rightIntake.spin(directionType::fwd, inTakeSpeed,velocityUnits::pct);
+    leftIntake.spin(directionType::fwd, -inTakeSpeed,velocityUnits::pct);
+    task::sleep(20);
+    return;
+}
+
+void stopIntakes()
+{
+    rightIntake.spin(directionType::fwd, 0,velocityUnits::pct);
+    leftIntake.spin(directionType::fwd, 0,velocityUnits::pct);
+    task::sleep(20);
+    return;
+}
+
+void moveIntakesUp()
+{
+    liftArm.rotateFor(liftIntakesSpeed, timeUnits::sec, topSpeed, velocityUnits::pct);
+    task::sleep(20);
+    return;
+}
+
+void moveIntakesDown()
+{
+    liftArm.rotateFor(liftIntakesSpeed, timeUnits::sec, -topSpeed, velocityUnits::pct);
+    task::sleep(20);
     return;
 }
 
@@ -33,20 +66,18 @@ int main()
 {
   Controller1.ButtonL1.pressed(moveRampUp);
   Controller1.ButtonL2.pressed(moveRampDown);
+  Controller1.ButtonUp.pressed(moveIntakesUp);
+  Controller1.ButtonDown.pressed(moveIntakesDown);
+  Controller1.ButtonR1.pressed(spinIntakes);
+  Controller1.ButtonR2.pressed(stopIntakes);
   while(true)
   {
     //
-    leftBackMotor.spin(directionType::fwd, (Controller1.Axis3.value()),velocityUnits::pct);
-    leftMiddleMotor.spin(directionType::fwd, (Controller1.Axis3.value()),velocityUnits::pct);
+    leftBackMotor.spin(directionType::fwd, -(Controller1.Axis3.value()),velocityUnits::pct);
+    leftMiddleMotor.spin(directionType::fwd, -(Controller1.Axis3.value()),velocityUnits::pct);
 
     rightBackMotor.spin(directionType::fwd, (Controller1.Axis2.value()),velocityUnits::pct);
     rightMiddleMotor.spin(directionType::fwd, (Controller1.Axis2.value()),velocityUnits::pct);
-
-    if(Controller1.ButtonR1.pressing()) 
-    {
-        rightIntake.spin(directionType::fwd, inTakeSpeed,velocityUnits::pct);
-        leftIntake.spin(directionType::fwd, inTakeSpeed,velocityUnits::pct);
-    }
     task::sleep(20);
   }
 }
