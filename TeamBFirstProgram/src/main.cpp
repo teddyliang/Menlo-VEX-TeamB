efc599a72f3da596dfe3c0e06053ea9131c901ae
Alexandros Konas <alexkonas@gmail.com>
2019-10-26 13:56:43 -0700

    yes

--- TeamBFirstProgram/src/main.cpp
+++ TeamBFirstProgram/src/main.cpp
@@ -35,11 +35,28 @@ int inTakeSpeed = 50;
 double rampMovementTime = 0.7;
 int topSpeed = 50;
 int liftIntakesSpeed = 10;
+<<<<<<< HEAD
+bool slowMode = false;
+=======
+>>>>>>> bb3741206eaf82487c8b3e954692e3af6b09713f
 // A global instance of vex::brain used for printing to the V5 brain screen
 
 // define your global instances of motors and other devices here
 
 
+<<<<<<< HEAD
+void moveRampUp()
+{
+  Ramp.spinFor(2, timeUnits::sec, topSpeed,velocityUnits::pct);
+  return;
+}
+
+void moveRampDown()
+{
+  Ramp.spinFor(4, timeUnits::sec, -topSpeed,velocityUnits::pct);
+  return;
+}
+=======
 void moveRamp()
 {
     if (Controller1.ButtonL1.pressing())
@@ -57,6 +74,7 @@ void moveRamp()
 
 }
 
+>>>>>>> bb3741206eaf82487c8b3e954692e3af6b09713f
 void spinIntakes()
 {
     RightIntake.spin(directionType::fwd, -inTakeSpeed,velocityUnits::pct);
@@ -67,12 +85,48 @@ void spinIntakes()
 
 void stopIntakes()
 {
+<<<<<<< HEAD
+    slowMode = false;
+=======
+>>>>>>> bb3741206eaf82487c8b3e954692e3af6b09713f
     RightIntake.spin(directionType::fwd, 0,velocityUnits::pct);
     LeftIntake.spin(directionType::fwd, 0,velocityUnits::pct);
     task::sleep(20);
     return;
 }
 
+<<<<<<< HEAD
+void reverseIntakes()
+{
+    slowMode = true;
+    RightIntake.spin(directionType::rev, -inTakeSpeed/10,velocityUnits::pct);
+    LeftIntake.spin(directionType::rev, inTakeSpeed/10,velocityUnits::pct);
+    task::sleep(20);
+    return;
+}
+
+// void dumpStack()
+// {
+//   RightIntake.spin(directionType::rev, -inTakeSpeed/10,velocityUnits::pct);
+//   LeftIntake.spin(directionType::rev, inTakeSpeed/10,velocityUnits::pct);
+
+//   LeftBack.spinFor(4, timeUnits::sec, topSpeed,velocityUnits::pct);
+//   LeftMiddle.spinFor(4, timeUnits::sec, topSpeed,velocityUnits::pct);
+
+//   // LeftBack.spin(directionType::fwd, topSpeed,velocityUnits::pct);
+//   // LeftMiddle.spin(directionType::fwd, topSpeed,velocityUnits::pct);
+
+//   LeftBack.spinFor(4, timeUnits::sec, -topSpeed,velocityUnits::pct);
+//   LeftMiddle.spinFor(4, timeUnits::sec,- topSpeed,velocityUnits::pct);
+
+//   // RightBack.spin(directionType::fwd, -topSpeed,velocityUnits::pct);
+//   // RightMiddle.spin(directionType::fwd, -topSpeed,velocityUnits::pct);
+//   task::sleep(5);
+//   return;
+// }
+
+=======
+>>>>>>> bb3741206eaf82487c8b3e954692e3af6b09713f
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
@@ -84,7 +138,11 @@ int main() {
     {
       LiftArm.spin(forward);
     } 
+<<<<<<< HEAD
+    else if (Controller1.ButtonDown.pressing())
+=======
     else if (Controller1.ButtonB.pressing())
+>>>>>>> bb3741206eaf82487c8b3e954692e3af6b09713f
     {
       LiftArm.spin(reverse);
     }
@@ -93,6 +151,33 @@ int main() {
       LiftArm.stop();
     }
 
+<<<<<<< HEAD
+    Controller1.ButtonL1.pressed(moveRampUp);
+    Controller1.ButtonL2.pressed(moveRampDown);
+
+    Controller1.ButtonX.pressed(spinIntakes);
+    Controller1.ButtonY.pressed(stopIntakes);
+    Controller1.ButtonB.pressed(reverseIntakes);
+
+    if(!slowMode)
+    {
+      LeftBack.spin(directionType::fwd, -(Controller1.Axis3.value()),velocityUnits::pct);
+      LeftMiddle.spin(directionType::fwd, -(Controller1.Axis3.value()),velocityUnits::pct);
+
+      RightBack.spin(directionType::fwd, (Controller1.Axis2.value()),velocityUnits::pct);
+      RightMiddle.spin(directionType::fwd, (Controller1.Axis2.value()),velocityUnits::pct);
+      task::sleep(20);
+    }
+    else
+    {
+      LeftBack.spin(directionType::fwd, -(Controller1.Axis3.value()/5),velocityUnits::pct);
+      LeftMiddle.spin(directionType::fwd, -(Controller1.Axis3.value()/5),velocityUnits::pct);
+
+      RightBack.spin(directionType::fwd, (Controller1.Axis2.value()/5),velocityUnits::pct);
+      RightMiddle.spin(directionType::fwd, (Controller1.Axis2.value()/5),velocityUnits::pct);
+      task::sleep(20);
+    }
+=======
     
     Controller1.ButtonL1.pressed(moveRampUp);
     Controller1.ButtonL2.pressed(moveRampDown);
@@ -107,5 +192,6 @@ int main() {
     RightBack.spin(directionType::fwd, (Controller1.Axis2.value()),velocityUnits::pct);
     RightMiddle.spin(directionType::fwd, (Controller1.Axis2.value()),velocityUnits::pct);
     task::sleep(20);
+>>>>>>> bb3741206eaf82487c8b3e954692e3af6b09713f
   }
 }
